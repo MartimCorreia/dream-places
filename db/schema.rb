e74ec6e2088ad3d720803cbf1f60dbb9904c76fb
Martim Correia <correia_martim@hotmail.com>
2023-02-03 08:46:16 +0000

    migration about_me instance cariable to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_090448) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_03_084505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_090448) do
     t.datetime "updated_at", null: false
     t.string "firstName"
     t.string "lastName"
+    t.string "about_me"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
